695430bae27343744b90eae6e58b177002468163
TranHaGiaHuy <huythgce170636@fpt.edu.vn>
2024-07-18 09:26:21 +0700

    CHECK

---
+++ wwwroot/js/sorting.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+	$('#filterButton').click(function () {
+		var selectedCategories = [];
+		$('input[name="categoryCheckbox"]:checked').each(function () {
+			selectedCategories.push($(this).val());
+		});
+
+		$.ajax({
+			url: '@Url.Page("./Index", "GetFilter")',
+			type: 'GET',  // Đổi sang 'GET' nếu sử dụng OnGetFilter
+			data: { filRes: selectedCategories },
+			success: function (data) {
+				$('#resultsContainer').html(data);  // Cập nhật phần tử với dữ liệu trả về
+			},
+			error: function (xhr, status, error) {
+				console.error('AJAX Error:', status, error);  // Xử lý lỗi nếu có
+			}
+		});
+	});
+});
